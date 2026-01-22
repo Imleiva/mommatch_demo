6ea13af9cbfd2c2c30a3c77e3ffe70e66a42e9c4
Imleiva <ImLeiva@github.com>
2026-01-22 21:12:29 +0100

    Updates

--- frontend/src/components/Chat.js
+++
@@ -1,1 +0,0 @@
-// This file has been removed or commented out as per the suggested code change.
--- frontend/src/setupTests.js
+++
@@ -1,8 +0,0 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom";
-
-// Configuración básica para mis tests. Lo dejé tal cual lo genera create-react-app
-// porque funciona bien. Algún diía debería investigar más sobre jest-dom, pero
-// mientras tanto esto hace su trabajo bien.
